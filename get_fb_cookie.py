7257dedef90f5052a825112ff560ec4b3b9723f2
Ujjawal Kumar <107112307+affnarayani@users.noreply.github.com>
2026-07-31 16:50:38 +0530

    31072026

--- get_fb_cookie.py
+++ get_fb_cookie.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import time
+import random
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 from playwright_stealth import Stealth
@@ -72,8 +73,11 @@ def perform_login(page, email, password):
     page.goto(FACEBOOK_URL, wait_until="domcontentloaded")
 
     page.get_by_role("textbox", name="Email address or mobile number").fill(email)
+    time.sleep(random.uniform(6, 12))
     page.get_by_role("textbox", name="Password").fill(password)
+    time.sleep(random.uniform(6, 12))
     page.get_by_role("button", name="Log in").click()
+    time.sleep(random.uniform(6, 12))
 
 
 # -------------------- WAIT LOGIN --------------------
